3f6d3f9ab315320943f3e5e5d78412b496b20cb1
Will Meister <meisterwilliam5@gmail.com>
2023-01-30 13:36:48 -0500

    installed firebase tools locally

---
+++ app.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4ZHv_x_raRsGmoiYLy26aNfd8NRAdWvI",
+    authDomain: "bielakmeister-webdev.firebaseapp.com",
+    projectId: "bielakmeister-webdev",
+    storageBucket: "bielakmeister-webdev.appspot.com",
+    messagingSenderId: "56939773881",
+    appId: "1:56939773881:web:331ac6a765027298110068"
+  };
+
+const app = initializeApp(firebaseConfig);
